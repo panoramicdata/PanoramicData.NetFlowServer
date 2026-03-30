b703a8453dd8c025989ba3355576c778410ab0f6
David Bond <david@davidbond.net>
2026-03-30 12:22:23 +0100

    NuGet governance: central pkg mgmt, Directory.Build.props, CI/CD, update deps, nullable fixes

--- ExampleApp/CommandService.cs
+++ ExampleApp/CommandService.cs
@@ -7,8 +7,8 @@ namespace ExampleApp;
 
 public class CommandService
 {
-	private Process _process = null;
-	private ProcessStartInfo _startInfo = null;
+	private Process? _process;
+	private readonly ProcessStartInfo _startInfo;
 
 	public CommandService(string command, string args)
 	{
@@ -22,35 +22,35 @@ public class CommandService
 		};
 	}
 
-	public event EventHandler<byte[]> DataReceived;
-	public event EventHandler EofReceived;
-	public event EventHandler<uint> CloseReceived;
+	public event EventHandler<byte[]>? DataReceived;
+	public event EventHandler? EofReceived;
+	public event EventHandler<uint>? CloseReceived;
 
 	public void Start()
 	{
-		_process = Process.Start(_startInfo);
+		_process = Process.Start(_startInfo) ?? throw new InvalidOperationException("Failed to start process.");
 		Task.Run(() => MessageLoop());
 	}
 
 	public void OnData(byte[] data)
 	{
-		_process.StandardInput.BaseStream.Write(data, 0, data.Length);
+		_process!.StandardInput.BaseStream.Write(data, 0, data.Length);
 		_process.StandardInput.BaseStream.Flush();
 	}
 
-	public void OnClose() => _process.StandardInput.BaseStream.Close();
+	public void OnClose() => _process!.StandardInput.BaseStream.Close();
 
 	private void MessageLoop()
 	{
 		var bytes = new byte[1024 * 64];
 		while (true)
 		{
-			var len = _process.StandardOutput.BaseStream.Read(bytes, 0, bytes.Length);
+			var len = _process!.StandardOutput.BaseStream.Read(bytes, 0, bytes.Length);
 			if (len <= 0)
 				break;
 
 			var data = bytes.Length != len
-				? bytes.Take(len).ToArray()
+				? [.. bytes.Take(len)]
 				: bytes;
 			DataReceived?.Invoke(this, data);
 		}
--- README.md
+++ README.md
@@ -1,9 +1,9 @@
 ## Panoramic Data NetFlow Server
-Panoramic Data NetFlow Server is a lightweight [SSH](http://en.wikipedia.org/wiki/NetFlow) server side application. 
 
----
-### Nuget
-[![NuGet version](https://badge.fury.io/nu/FxSsh.svg)](https://www.nuget.org/packages/PanoramicData.NetFlowServer/)
+[![Codacy Badge](https://app.codacy.com/project/badge/Grade/735072bef30049279f73514724f325a2)](https://app.codacy.com/gh/panoramicdata/PanoramicData.NetFlowServer/dashboard?utm_source=gh&utm_medium=referral&utm_content=&utm_campaign=Badge_grade)
+[![NuGet version](https://badge.fury.io/nu/PanoramicData.NetFlowServer.svg)](https://www.nuget.org/packages/PanoramicData.NetFlowServer/)
+
+Panoramic Data NetFlow Server is a lightweight [NetFlow](http://en.wikipedia.org/wiki/NetFlow) server side application.
 
 `PM> Install-Package PanoramicData.NetFlowServer`
 
